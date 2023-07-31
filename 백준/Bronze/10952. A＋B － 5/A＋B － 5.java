c5b73b93ad59f01a5f161fa669f9eddb7e13cbb6
taexee <134465372+Taexee@users.noreply.github.com>
2023-07-31 22:52:32 +0900

    [Bronze V] Title: A+B - 5, Time: 224 ms, Memory: 17840 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		while(true) {
+		
+			int a = in.nextInt();
+			int b = in.nextInt();
+			
+			if(a == 0 && b == 0) {
+				break;
+			} else {
+				System.out.println(a + b);
+			}
+		}
+		
+	}
+}
