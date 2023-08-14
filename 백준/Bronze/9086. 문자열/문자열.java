e41f30e6cfd77b5cd8bf25c54eb0f9b9d7970ab7
taexee <134465372+Taexee@users.noreply.github.com>
2023-08-15 08:40:06 +0900

    [Bronze V] Title: 문자열, Time: 236 ms, Memory: 18332 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int count = sc.nextInt();
+		
+		for(int i = 0; i < count; i++) {
+			String str = sc.next();
+			System.out.println(str.charAt(0)+""+str.charAt(str.length()-1));
+		}
+		sc.close();
+	}
+}
