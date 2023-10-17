5cfb6a18be62bf5219abcd54819985b12cbce141
taexee <134465372+Taexee@users.noreply.github.com>
2023-10-17 09:29:48 +0900

    [level unrated] Title: 더 크게 합치기, Time: 11.22 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int solution(int a, int b) {
+        int result1 = Integer.parseInt(a + "" + b);
+        int result2 = Integer.parseInt(b + "" + a);
+        return Math.max(result1, result2);
+    }
+}
