f3780abce02b026901be784afc22b9fbf1711fe7
taexee <134465372+Taexee@users.noreply.github.com>
2023-10-20 13:45:43 +0900

    [level unrated] Title: 진료 순서 정하기, Time: 0.01 ms, Memory: 71.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120835. 진료 순서 정하기/README.md
+++ 프로그래머스/unrated/120835. 진료 순서 정하기/README.md
@@ -1,10 +1,10 @@
 # [level unrated] 진료 순서 정하기 - 120835 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120835) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120835?language=java) 
 
 ### 성능 요약
 
-메모리: 73.4 MB, 시간: 0.02 ms
+메모리: 71.1 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 5일 13:41:51
+2023년 10월 5일 13:45:41
 
 ### 문제 설명
 
--- 프로그래머스/unrated/120835. 진료 순서 정하기/진료 순서 정하기.java
+++ 프로그래머스/unrated/120835. 진료 순서 정하기/진료 순서 정하기.java
@@ -2,14 +2,17 @@ class Solution {
     public int[] solution(int[] emergency) {
         int[] answer = new int[emergency.length];
 
-        // 높을수록 응급도 높음
-        // 3, 1, 2 출력
-        for(int i = 0; i < emergency.length; i++) {
-            answer[i]++; // 순서 1부터 시작 모두 1 더해줌
-            for(int j = 0; j < emergency.length; j++) {
-                if (emergency[i] < emergency[j])
-                    answer[i]++;
+        for(int i = 0; i < answer.length; i++){
+            if(answer[i] != 0){
+                continue;
             }
+            int idx = 1;
+            for(int j = 0; j < answer.length; j++){
+                if(emergency[i] < emergency[j]){
+                    idx++;
+                }
+            }
+            answer[i] = idx;
         }
         return answer;
     }
