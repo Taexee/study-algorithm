8404902fff506e5e5f45f4b34830d61725aaa390
taexee <134465372+Taexee@users.noreply.github.com>
2023-10-23 13:42:21 +0900

    [level unrated] Title: 팩토리얼, Time: 0.03 ms, Memory: 72 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120848. 팩토리얼/팩토리얼.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i = 1; i <= 10; i++){
+            if(factorial(i) <= n) answer = i;
+        }
+
+        return answer;
+    }
+    public int factorial(int n) {
+        return (n<=1) ? n : factorial(n-1)*n;
+    }
+}
