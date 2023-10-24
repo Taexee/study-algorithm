20da8c8ad49d782ed831b71c3c74adda4bff13bf
taexee <134465372+Taexee@users.noreply.github.com>
2023-10-25 00:52:08 +0900

    [level unrated] Title: 모음 제거, Time: 0.06 ms, Memory: 81.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120849. 모음 제거/모음 제거.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String my_string) {
+        String[] vowel = {"a", "e", "i", "o", "u"};
+        for(int i = 0; i < vowel.length; i++){
+            if(my_string.contains(vowel[i])){
+                my_string = my_string.replace(vowel[i], "");
+            }
+        }
+        return my_string;
+    }
+}
