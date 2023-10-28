78637551860eac004db45ccf8c97218df7fdb9d7
taexee <134465372+Taexee@users.noreply.github.com>
2023-10-28 21:33:58 +0900

    [level unrated] Title: 가장 큰 수 찾기, Time: 0.01 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/unrated/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
+++ 프로그래머스/unrated/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -1,21 +1,14 @@
-import java.util.Arrays;
 class Solution {
     public int[] solution(int[] array) {
         int[] answer = new int[2];
 
-        int max = 0;
-        for(int i = 0; i < array.length; i++){
-
-            if(array[i] >= max) {
-                max = array[i];
+        for(int i=0;i<array.length;i++) {
+            if(array[i] > answer[0]) {
+                answer[0] = array[i];
                 answer[1] = i;
             }
-            System.out.println(array[i] +" "+ max);
         }
-        Arrays.sort(array);
 
-        answer[0] = array[array.length-1];
-        
         return answer;
     }
 }
