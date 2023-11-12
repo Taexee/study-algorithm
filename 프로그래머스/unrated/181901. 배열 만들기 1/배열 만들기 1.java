5ac688ca64af34123de7ae89054ccd99caa14beb
Taexee <134465372+Taexee@users.noreply.github.com>
2023-11-13 04:31:14 +0900

    [level unrated] Title: 배열 만들기 1, Time: 0.03 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.java
+++ 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.java
@@ -1,9 +1,10 @@
 class Solution {
     public int[] solution(int n, int k) {
-        int[] answer = new int[n/k];
-        int idx = 0;
-        for(int j = 1; j <= n; j++){
-            if(j%k == 0) answer[idx++] = j;
+        int count = n / k; 
+        int[] answer = new int[count];
+
+        for (int i = 1; i <= count; i++) {
+            answer[i - 1] = k * i;
         }
         return answer;
     }
