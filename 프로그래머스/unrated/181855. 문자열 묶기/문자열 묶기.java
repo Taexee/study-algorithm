87249c74d91de5066d12d5e9db512a6ed5dea63d
Taexee <134465372+Taexee@users.noreply.github.com>
2023-11-23 04:33:49 +0900

    [level unrated] Title: 문자열 묶기, Time: 6.39 ms, Memory: 122 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181855. 문자열 묶기/문자열 묶기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(String[] strArr) {
+        int answer = 0;
+        int[] count = new int[100000];
+
+        for(int i = 0; i < strArr.length; i++){
+            count[strArr[i].length()]++;
+        }
+        
+        for(int cnt : count){
+            if(cnt > answer) answer = cnt;
+        }
+        return answer;
+    }
+}
