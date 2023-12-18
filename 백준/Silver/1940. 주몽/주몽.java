b4b3d37c544b85099a31fb61c740299fab434be0
Taexee <134465372+Taexee@users.noreply.github.com>
2023-12-19 00:46:49 +0900

    [Silver IV] Title: 주몽, Time: 200 ms, Memory: 16540 KB -BaekjoonHub

---
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int count = 0;
+        int i = 0; // startIdx -> min
+        int j = n-1; //endIdx -> max
+
+        while (i < j){
+            if(arr[i] + arr[j] < m) {
+                i++;
+            } else if (arr[i] + arr[j] > m) {
+                j--;
+            } else {
+                count++;
+                i++;
+                j--;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
