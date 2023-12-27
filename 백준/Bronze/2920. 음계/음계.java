2588a76eb5d1122135d2cd60588d67a258a235f1
Taexee <134465372+Taexee@users.noreply.github.com>
2023-12-27 17:32:01 +0900

    [Bronze II] Title: 음계, Time: 224 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[8];
+
+        for(int i = 0; i < arr.length; i++) {
+           arr[i] = sc.nextInt();
+        }
+
+        String answer = "";
+        for(int i = 0; i < arr.length - 1; i++){
+            if(arr[i]+1 == arr[i+1]) {
+                answer = "ascending";
+            } else if (arr[i+1] == arr[i]-1) {
+                answer = "descending";
+            } else {
+                answer = "mixed";
+                break;
+            }
+        }
+
+        System.out.println(answer);
+
+    }
+}
