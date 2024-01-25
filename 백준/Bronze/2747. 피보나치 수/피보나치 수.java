09a1baccda6c5ba034fbbd72851eb9ac17ea536b
Taexee <134465372+Taexee@users.noreply.github.com>
2024-01-25 14:06:26 +0900

    [Bronze II] Title: 피보나치 수, Time: 124 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] a = new int[46];
+        a[1] = 1;
+
+        int n = Integer.parseInt(br.readLine());
+        for(int i = 2; i <= n; i++) {
+            a[i] = a[i-1] + a[i-2];
+        }
+        System.out.println(a[n]);
+    }
+}
