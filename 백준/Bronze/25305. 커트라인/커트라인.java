28addec79e3f15570f2cb872601fe15a706812d6
Taexee <134465372+Taexee@users.noreply.github.com>
2024-03-19 17:19:31 +0900

    [Bronze II] Title: 커트라인, Time: 132 ms, Memory: 14488 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st =  new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken()); 
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] score = new int[N];
+
+        st =  new StringTokenizer(br.readLine()); 
+
+        for(int i = 0; i < N; i++) { // 점수 5개
+            score[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        Arrays.sort(score);
+
+        bw.write(score[N-k] +""); 
+        bw.flush();
+        bw.close();
+
+    }
+}
